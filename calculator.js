f7449cd326965ed8ededbd7cf8df7d51cc390bdf
ANKUSH0480 <rajankush0480@gmail.com>
2025-12-22 14:14:31 +0530

    1st commit

---
+++ calculator.js
@@ -0,0 +1,17 @@
+/* create a faulty calculator using java script 
+ 
+this calculator does following :- 
+1 . it take two no as input from the user 
+2. it perform wrong opertion as follow : 
+ +----> - 
+ *-----> +
+ -   ---> / 
+ / -----> ** 
+
+it perform wrong operation 10% of the time 
+
+
+*/  
+
+maths.random()
+ 
