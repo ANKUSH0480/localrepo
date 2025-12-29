30b5ab021bee8707f1227a714f078c43431d429b
ANKUSH0480 <rajankush0480@gmail.com>
2025-12-29 15:17:12 +0530

    29/12 work

---
+++ question2.js
@@ -0,0 +1,7 @@
+let newBtn = document.createElement("button");
+newBtn.innerText = "Click Me!";
+newBtn.style.backgroundColor = "blue";
+newBtn.style.color = "red";
+document.querySelector("body").prepend(newBtn);
+
+
